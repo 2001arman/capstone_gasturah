7952d04007762913ac99ca91849f0fcb58739d6a
Lasteinsa <mikueinsa@gmail.com>
2022-05-29 18:10:31 +0700

    maps edit

--- app/src/main/java/com/gasturah/ui/location/MapsActivity.kt
+++ app/src/main/java/com/gasturah/ui/location/MapsActivity.kt
@@ -1,7 +1,22 @@
 package com.gasturah.ui.location
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.EditText
+import android.widget.SearchView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.content.ContextCompat
+import com.gasturah.R
+import com.gasturah.databinding.ActivityMapsBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,12 +24,15 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.gasturah.ui.location.databinding.ActivityMapsBinding
+import java.io.IOException
+import java.util.concurrent.Executors
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private val executor = Executors.newSingleThreadExecutor()
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,27 +40,84 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        setupView()
+        setupAction()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        mMap = googleMap;
+        getMyLocation()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun setupView() {
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.mapsSearchEdit.setOnClickListener {
+            binding.mapsSearchEdit.onActionViewExpanded()
+        }
+        binding.mapsSearchEdit.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String): Boolean {
+                executor.execute {
+                    searchLocation(p0)
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String): Boolean {
+                return false
+            }
+
+        })
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                getMyLocation()
+            }
+        }
+
+    private fun getMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun searchLocation(p0: String){
+
+        val location: String = p0
+        var addressList: List<Address>? = null
+
+        if (location.isEmpty()){
+            Toast.makeText(this, "provide location", Toast.LENGTH_SHORT).show()
+        }else{
+            val geoCoder    = Geocoder(this)
+            addressList     = geoCoder.getFromLocationName(location, 1)
+
+            handler.post {
+                if(addressList.isEmpty()) {
+                    Toast.makeText(this, "Not found", Toast.LENGTH_SHORT).show()
+                } else {
+                    val address = addressList[0]
+                    val latLng  = LatLng(address.latitude, address.longitude)
+                    mMap.addMarker(MarkerOptions().position(latLng).title(location))
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+                }
+            }
+        }
     }
 }
