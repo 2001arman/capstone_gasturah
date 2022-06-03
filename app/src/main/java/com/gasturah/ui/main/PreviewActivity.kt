7cd031415319e76d83ad9c72fc8ebeb434ef2bb3
Lasteinsa <mikueinsa@gmail.com>
2022-06-03 19:07:24 +0700

    design and setup preview

--- app/src/main/java/com/gasturah/ui/main/PreviewActivity.kt
+++ app/src/main/java/com/gasturah/ui/main/PreviewActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.gasturah.databinding.ActivityPreviewBinding
 import java.io.FileInputStream
 
@@ -16,6 +18,7 @@ class PreviewActivity : AppCompatActivity() {
         binding = ActivityPreviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide()
         getImage()
     }
 
@@ -26,6 +29,11 @@ class PreviewActivity : AppCompatActivity() {
             val bmp: Bitmap = BitmapFactory.decodeStream(`is`)
             `is`.close()
             binding.imagePreview.setImageBitmap(bmp)
+
+            Glide.with(this).asBitmap()
+                .load(bmp)
+                .transform(RoundedCorners(32))
+                .into(binding.imagePreview)
         } catch (e: Exception) {
             e.printStackTrace()
         }
