c4e825969c9f79308bc09c5ed5f348625c768319
Arman <2001armanmaulana@gmail.com>
2022-06-04 00:29:31 +0800

    comment button

--- app/src/main/java/com/gasturah/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gasturah/ui/home/HomeFragment.kt
@@ -1,17 +1,15 @@
 package com.gasturah.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.gasturah.databinding.FragmentHomeBinding
-import com.gasturah.ui.login.LoginActivity
-import com.gasturah.ui.main.DetailActivity
+
 
 class HomeFragment : Fragment() {
 
@@ -20,6 +18,24 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        val imageView = ImageView(context)
+//        // setting height and width of imageview
+//        imageView.layoutParams = LinearLayout.LayoutParams(400, 400)
+//        imageView.x = 20F //setting margin from left
+//        imageView.y = 20F
+//        val uri = "@drawable/rekomendasi_borobudur.png"
+//        val imageResource = resources.getIdentifier(uri, null, getActivity()?.getPackageName())
+//        val res = resources.getDrawable(imageResource)
+//        imageView.setImageDrawable(res)
+//        val layout = binding.relativeRekomendasi
+
+        // Add ImageView to LinearLayout
+//        layout?.addView(imageView)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,17 +48,18 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
-        // Navigate to LoginPage. Well, ignore it. for testing purpose
-        val button: Button = binding.btnDeskripsi
-        button.setOnClickListener{
-            val moveToLogin = Intent(context, DetailActivity::class.java)
-            startActivity(moveToLogin)
-        }
+//        LinearLayout myRoot = (LinearLayout)findViewById(binding.linearRekomendasi)
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+//
+//        // Navigate to LoginPage. Well, ignore it. for testing purpose
+//        val button: Button = binding.btnDeskripsi
+//        button.setOnClickListener{
+//            val moveToLogin = Intent(context, DetailActivity::class.java)
+//            startActivity(moveToLogin)
+//        }
         return root
     }
 
