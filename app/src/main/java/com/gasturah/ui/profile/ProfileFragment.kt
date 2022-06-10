e65c65a2dcb13e2b63a3a2acc960c4cb089aefb9
Arman <2001armanmaulana@gmail.com>
2022-06-10 17:55:05 +0800

    ui profile fragment

--- app/src/main/java/com/gasturah/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/gasturah/ui/profile/ProfileFragment.kt
@@ -1,19 +1,24 @@
 package com.gasturah.ui.profile
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.gasturah.data.util.ModelPreferencesManager
 import com.gasturah.databinding.FragmentProfileBinding
-import com.gasturah.ui.login.LoginActivity
+import com.gasturah.model.UserModel
+import com.gasturah.ui.home.RecyclerPostingAdapter
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private val baseurl: String = ApiConfig.baseUrl
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,11 +31,27 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnLogout.setOnClickListener {
-            ModelPreferencesManager.preferences.edit().remove("user").commit()
-            var intent = Intent(context, LoginActivity::class.java)
-            startActivity(intent);
-            activity?.finish()
+//        binding.btnLogout.setOnClickListener {
+//            ModelPreferencesManager.preferences.edit().remove("user").commit()
+//            var intent = Intent(context, LoginActivity::class.java)
+//            startActivity(intent);
+//            activity?.finish()
+//        }
+        val user = ModelPreferencesManager.get<UserModel>("user")
+        val horizontalLayoutManagaer =
+            LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        binding.apply {
+            recyclerPosting.apply {
+                setHasFixedSize(true)
+                this.layoutManager = horizontalLayoutManagaer
+
+            }
+            profileSection.tvName.text = user!!.name
+            profileSection.tvLevel.text = user!!.level
+            Glide.with(this@ProfileFragment)
+                .load(baseurl + user!!.profile_picture)
+                .into(profileSection.imgProfile)
+            binding.recyclerPosting.adapter = RecyclerPostingAdapter()
         }
     }
 }
