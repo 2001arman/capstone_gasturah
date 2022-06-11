883499c5f7db975ad0ded612effadf417517e4bc
Arman <2001armanmaulana@gmail.com>
2022-06-11 13:49:48 +0800

    fix api

--- app/src/main/java/com/gasturah/service/ApiConfig.kt
+++ app/src/main/java/com/gasturah/service/ApiConfig.kt
@@ -1,12 +1,17 @@
 import com.gasturah.service.ApiService
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 class ApiConfig {
     companion object{
-        const val baseUrl = "http://20.89.151.13/"
+        const val baseUrl = "https://gasturah.000webhostapp.com/"
+        private var gson = GsonBuilder()
+            .setLenient()
+            .create()
         fun getApiService(): ApiService {
             val loggingInterceptor =
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
@@ -15,7 +20,7 @@ class ApiConfig {
                 .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl(baseUrl)
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(client)
                 .build()
             return retrofit.create(ApiService::class.java)
