b94b84c46e3a90ae65d983ccf9ad0d51fb1ceabf
Arman <2001armanmaulana@gmail.com>
2022-06-12 15:45:38 +0800

    add data to ui

--- app/src/main/java/com/gasturah/ui/home/RecyclerPostingAdapter.kt
+++ app/src/main/java/com/gasturah/ui/home/RecyclerPostingAdapter.kt
@@ -3,13 +3,18 @@ package com.gasturah.ui.home
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.gasturah.R
 import com.gasturah.databinding.LayoutPostingImageBinding
 import com.gasturah.databinding.LayoutProfileSectionBinding
+import com.gasturah.response.ContentItem
+import com.gasturah.response.ContentPosting
 
-class RecyclerPostingAdapter : RecyclerView.Adapter<RecyclerPostingAdapter.ViewHolder>() {
+class RecyclerPostingAdapter(private val listPosting: List<ContentPosting>) : RecyclerView.Adapter<RecyclerPostingAdapter.ViewHolder>() {
 
     inner class ViewHolder(val binding: LayoutPostingImageBinding) : RecyclerView.ViewHolder(binding.root)
+    private val baseurl: String = ApiConfig.baseUrl
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -19,24 +24,22 @@ class RecyclerPostingAdapter : RecyclerView.Adapter<RecyclerPostingAdapter.ViewH
         return ViewHolder(binding)
     }
 
-    inner class LandmarkWithImageViewHolder(private val profileSectionBinding: LayoutProfileSectionBinding) :
-        RecyclerView.ViewHolder(profileSectionBinding.root) {
-    }
 
-    val username = listOf<String>(
-        "Arman Maulana", "Eko Darmawan", "Daffa Maffazi"
-    )
     override fun onBindViewHolder(holder: RecyclerPostingAdapter.ViewHolder, position: Int) {
-        val user = username[position]
+        val postingan = listPosting[position]
         holder.apply {
             binding.apply {
-                tvUsername.setText(user)
-                imagePosting.setImageResource(R.drawable.lawangsewu)
-                imgAvatarUser.setImageResource(R.drawable.rekomendasi_borobudur)
+                tvUsername.setText(postingan.user)
+                Glide.with(this.root)
+                    .load(baseurl + postingan.foto)
+                    .into(imagePosting)
+                Glide.with(this.root)
+                    .load(baseurl + postingan.profilePicture)
+                    .into(imgAvatarUser)
             }
         }
     }
 
-    override fun getItemCount() = username.size
+    override fun getItemCount() = listPosting.size
 
 }
