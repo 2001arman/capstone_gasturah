35227b07619b560310b6e0f933789bebe90a97c5
Arman <2001armanmaulana@gmail.com>
2022-06-12 19:21:49 +0800

    fix profile

--- app/src/main/java/com/gasturah/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/gasturah/ui/profile/ProfileFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
