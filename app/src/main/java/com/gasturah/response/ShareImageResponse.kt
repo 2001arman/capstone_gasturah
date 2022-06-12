9a2d8b3536e826be6cac02fe98c6e76975209d2c
Arman <2001armanmaulana@gmail.com>
2022-06-12 15:44:53 +0800

    new share image response

---
+++ app/src/main/java/com/gasturah/response/ShareImageResponse.kt
@@ -0,0 +1,19 @@
+package com.gasturah.response
+
+import com.google.gson.annotations.SerializedName
+import java.util.*
+
+data class ShareImageResponse(
+
+	@field:SerializedName("msg")
+	val msg: String,
+
+	@field:SerializedName("value")
+	val value: String,
+
+	@field:SerializedName("content")
+	val content: Objects,
+
+	@field:SerializedName("status")
+	val status: String
+)
